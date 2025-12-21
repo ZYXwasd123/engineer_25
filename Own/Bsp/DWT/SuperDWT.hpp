35c77eaa6ccf03b388cb724ee42d16528c59b0f7
abc/a-z <zhensh1924@mails.jlu.edu.cn>
2025-12-21 21:07:34 +0800

    slight change

--- Own/Bsp/DWT/SuperDWT.hpp
+++ Own/Bsp/DWT/SuperDWT.hpp
@@ -19,7 +19,7 @@ public:
     static void delay_us(uint32_t u_delay_time) {
         uint32_t start     = DWT->CYCCNT;
         uint32_t delay_cnt = u_delay_time * (SystemCoreClock / 1000000);
-        while (static_cast<uint32_t>(DWT->CYCCNT - start) < delay_cnt);
+        while (DWT->CYCCNT - start < delay_cnt);
     };
 
 private:
